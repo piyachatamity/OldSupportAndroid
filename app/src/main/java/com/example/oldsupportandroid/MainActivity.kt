da3f36c7e2f9f0dd37636ca362f89a75cddd44ab
Piyachat Jamsai <piyachat@amityeko.com>
2022-09-18 16:23:23 +0700

    add page activity

--- app/src/main/java/com/example/oldsupportandroid/MainActivity.kt
+++ app/src/main/java/com/example/oldsupportandroid/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.oldsupportandroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.hellolibrary.HelloMessage
+import com.example.hellolibrary.ui.PageHelloActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView: TextView = findViewById(R.id.text)
+        val button: Button = findViewById(R.id.btn)
 
         val text = HelloMessage().getMessage()
         textView.text = text
+
+        button.setOnClickListener {
+            openPageHello()
+        }
+    }
+
+    private fun openPageHello() {
+        val intent = Intent(this, PageHelloActivity::class.java)
+        startActivity(intent)
     }
 }

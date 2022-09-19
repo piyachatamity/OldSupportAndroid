991d7ecae53f5c53a4f609484ff055c59bdce452
Piyachat Jamsai <piyachat@amityeko.com>
2022-09-19 09:47:59 +0700

    add common module 0.0.1-alpha07

--- app/src/main/java/com/example/oldsupportandroid/MainActivity.kt
+++ app/src/main/java/com/example/oldsupportandroid/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.amityeko.rnr.common.base.BaseActivity
 import com.example.hellolibrary.HelloMessage
 import com.example.hellolibrary.new_ui.PageEmptyActivity
 
--- hellolibrary/src/main/java/com/example/hellolibrary/new_ui/PageEmptyActivity.kt
+++ hellolibrary/src/main/java/com/example/hellolibrary/new_ui/PageEmptyActivity.kt
@@ -2,11 +2,14 @@ package com.example.hellolibrary.new_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amityeko.rnr.common.base.BaseActivity
 import com.example.hellolibrary.R
 
 class PageEmptyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_empty)
+
+        val ac = BaseActivity()
     }
 }
